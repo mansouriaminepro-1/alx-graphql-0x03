6db3168bad85c7e83dc6e2a38f695f6841549a29
mansouriaminepro-1 <mansouriamine.pro@gmail.com>
2025-10-26 17:47:04 +0000

    Remove Sentry DSN from configuration

--- alx-rick-and-morty-app/sentry.client.config.ts
+++ alx-rick-and-morty-app/sentry.client.config.ts
@@ -2,7 +2,7 @@
 import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: 'https://a659a80e573dd46171c4f1c6879cd87a@o4510256190455808.ingest.de.sentry.io/4510256878387280',
+  dsn: '',
   tracesSampleRate: 1.0,
   replaysOnErrorSampleRate: 1.0,
   integrations: [],
