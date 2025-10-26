8b54e73ea308e8eba5acf9d1f35a925301156904
mansouriaminepro-1 <mansouriamine.pro@gmail.com>
2025-10-26 17:48:08 +0000

    Clear Sentry DSN in configuration
    Removed Sentry DSN for security reasons.

--- alx-rick-and-morty-app/sentry.edge.config.ts
+++ alx-rick-and-morty-app/sentry.edge.config.ts
@@ -6,7 +6,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://a659a80e573dd46171c4f1c6879cd87a@o4510256190455808.ingest.de.sentry.io/4510256878387280",
+  dsn: "",
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
